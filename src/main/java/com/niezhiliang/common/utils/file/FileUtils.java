8c278a1ab9caf586c68e7b6bcb6f235d1eed02d4
聂志良 <niezhiliang@huluwa.cc>
2018-11-28 08:49:41 +0800

    m

--- src/main/java/com/niezhiliang/common/utils/file/FileUtils.java
+++ src/main/java/com/niezhiliang/common/utils/file/FileUtils.java
@@ -2,9 +2,6 @@ package com.niezhiliang.common.utils.file;
 
 import com.niezhiliang.common.utils.random.CodeUtils;
 import com.niezhiliang.common.utils.random.UuidUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletOutputStream;
